63bd9ce75b65e618cf57b101a3b9e134dd55a64b
Dom Vu <Dominicvu36@gmail.com>
2020-03-30 21:51:02 -0600

    auth fix

--- auth_jwt.js
+++ auth_jwt.js
@@ -4,19 +4,20 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 require('dotenv').config();
 
-const opt = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken("jwt"),
-    secretOrKey: process.env.UNIQUE_KEY
-};
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+opts.secretOrKey = process.env.UNIQUE_KEY;
 
-passport.use(
-    new JwtStrategy(opt, function(jwt_payload, done) {
-        var user = db.find(jwt_payload.id);
-    if (user) {
-        done(null, user);
-    } else {
-        done(null, false);
+passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+        userMgr.findUserById(jwt_payload)
+            .then(
+                function (user) {
+                    if (user)
+                        done(null, user);
+                    else
+                        done(null, false);
+                });
     }
-}));
+));
 exports.isAuthenticated = passport.authenticate('jwt', { session : false });
 exports.secret = opts.secretOrKey ;
